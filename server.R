896140ca1da306f4fe2d6259f1b06fccdb341e49
alibuda <18833855855@139.com>
2016-03-03 01:11:44 +0800

    new

--- server.R
+++ server.R
@@ -1,13 +1,27 @@
 library(shiny)
 shinyServer(
     function(input, output) {
-        output$oid1 <- renderPrint({input$start_date})
-        output$oid2 <- renderPrint({input$end_date})
-        output$odate <- renderPrint({
-            if(input$start_date > input$end_date)
+        output$oid11 <- renderPrint({input$start_date1})
+        output$oid21 <- renderPrint({input$end_date1})
+        output$odate1 <- renderPrint({
+            if(input$start_date1 > input$end_date1)
                 "Start Date should be early !"
             else 
-                max(as.numeric(difftime(input$end_date,input$start_date,units = "days"))+1*input$id1+(input$id2-1),0)
+                max(as.numeric(difftime(input$end_date1,input$start_date1,units = "days"))+1*input$id11+(input$id21-1),0)
         })
+        output$oid12 <- renderPrint({
+            if(input$SE == "Start Date")
+                input$date
+            else
+                input$date - input$diff
+        })
+        output$oid22 <- renderPrint({
+            if(input$SE == "End Date")
+                input$date
+            else
+                input$date + input$diff
+        })
+        output$odate2 <- renderPrint({input$diff})
+
     }
 )
--- ui.R
+++ ui.R
@@ -1,19 +1,44 @@
 library(shiny)
-shinyUI(pageWithSidebar(
-    headerPanel("How many days between different Dates"),
-    sidebarPanel(
-        dateInput("start_date", "Start Date:"),
-        dateInput("end_date", "End Date:"),
-        checkboxInput("id1", "Including Start Date",F),
-        checkboxInput("id2", "Including End Date",T)
-    ),
-    mainPanel(
-        h3('Outputs'),
-        h4('Start Date:'),
-        verbatimTextOutput("oid1"),
-        h4('End Date:'),
-        verbatimTextOutput("oid2"),
-        h4('Day Diff:'),
-        verbatimTextOutput("odate")
-    )
-))
+
+shinyUI(navbarPage("How many days between different Dates",
+                   tabsetPanel(
+                       tabPanel("Documents",
+                                mainPanel(
+                                    includeHTML("Documents.html")
+                                )
+                       ),
+                       tabPanel("Output1",
+                                sidebarPanel(
+                                    dateInput("start_date1", "Start Date:"),
+                                    dateInput("end_date1", "End Date:"),
+                                    checkboxInput("id11", "Including Start Date",F),
+                                    checkboxInput("id21", "Including End Date",T)
+                                ),
+                                mainPanel(
+                                    h3('Output1'),
+                                    h4('Start Date:'),
+                                    verbatimTextOutput("oid11"),
+                                    h4('End Date:'),
+                                    verbatimTextOutput("oid21"),
+                                    h4('Day Diff:'),
+                                    verbatimTextOutput("odate1")
+                                )
+                       ),
+                       tabPanel("Output2",
+                                sidebarPanel(
+                                    selectInput("SE","Start Date/End Date",list("Start Date","End Date")),
+                                    dateInput("date", "Date:"),
+                                    numericInput("diff","Day Diff:",value = 0,min = 0,step = 1)
+                                ),
+                                mainPanel(
+                                    h3('Output2'),
+                                    h4('Start Date:'),
+                                    verbatimTextOutput("oid12"),
+                                    h4('End Date:'),
+                                    verbatimTextOutput("oid22"),
+                                    h4('Day Diff:'),
+                                    verbatimTextOutput("odate2")
+                                )
+                       )
+                   )
+))   
